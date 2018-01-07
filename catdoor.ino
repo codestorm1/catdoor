8b0ade56a48cd614eaa763bb60b9ef93127caefb
Bryan Green <twistedogre@gmail.com>
2018-01-07 00:49:56 -0800

    Small door movements in loop can be interrupted with button presses

--- catdoor.ino
+++ catdoor.ino
@@ -24,19 +24,23 @@ int ledPin = 13;
 Stepper motor(512, in1Pin, in3Pin, in2Pin, in4Pin);  
 int previous = 0;
 
-bool doorIsOpen = true;
-
 const int upButtonPin = 3; // button to open door, disables timer mode
 const int timerButtonPin = 4; // button to turn on timer mode
 const int downButtonPin = 5; // button to close door, also disables timer mode
 
-int upButtonState = 0;
-int downButtonState = 0;
-int timerButtonState = 0;
 bool timerMode = true;
 
 int stepsPerRevolution = 2048;
-int stepsPerPress = stepsPerRevolution * 6.25;
+int stepsToOpen = stepsPerRevolution * 6.25;
+int stepsPerLoop = stepsToOpen / 100;
+
+int openDoorPosition = 0;
+int closedDoorPosition = stepsToOpen;
+
+int doorPosition = 0; // initialize as though door is down
+
+bool doorInMotion = false;
+bool doorDirectionOpen = false;
 
 void digitalClockDisplay() {
   // digital clock display of the time
@@ -53,37 +57,45 @@ void printDigits(int digits) {
   Serial.print(digits);
 }
 
-void moveDoor(bool open) {
+void moveDoor() {
+  Serial.println("in Move door");
+    Serial.println(doorDirectionOpen);
+    Serial.println(doorPosition);
+    Serial.println(openDoorPosition);
+    Serial.println(closedDoorPosition);
+  if (doorDirectionOpen && (doorPosition > openDoorPosition)) {
+    Serial.println("Move door open");
+    motor.step(stepsPerLoop);
+    doorPosition += stepsPerLoop * -1;
+//    if (doorPosition <= openDoorPosition) {
+//      doorInMotion = false;
+//      digitalWrite(ledPin, LOW);
+//    }
+  } else if (!doorDirectionOpen && doorPosition < closedDoorPosition) {
+    Serial.println("Move door closed");
+    motor.step(stepsPerLoop * -1);
+    doorPosition += stepsPerLoop;
+//    if (doorPosition >= closedDoorPosition) {
+//      doorInMotion = false;
+//      digitalWrite(ledPin, LOW);
+//    }
+  } else {
+      doorInMotion = false;
+      digitalWrite(ledPin, LOW);
+  }
+}
+
+void startDoorMove(bool open) {
+  doorInMotion = true;
   digitalWrite(ledPin, HIGH);
-  int steps = open ? stepsPerPress : stepsPerPress * -1;
-  motor.step(steps);
-}
-
-void openDoor() {
-  if (!doorIsOpen) {
-    Serial.println("Opening door");
-    moveDoor(true);
-    doorIsOpen = true;
-    return;
-  }
-  Serial.println("Door is already open.");
-}
-
-void closeDoor() {
-  if (doorIsOpen) {
-    Serial.println("Closing door");
-    moveDoor(false);
-    doorIsOpen = false;
-    return;
-  }
-  Serial.println("Door is already closed.");
+  doorDirectionOpen = open;
 }
 
 void MorningOpen() {
   Serial.println("Open sez me");
   if (timerMode) {
     Serial.println("Time to open door");
-    openDoor();
+    startDoorMove(true);
   }
 }
 
@@ -91,7 +103,7 @@ void EveningClose() {
   Serial.println("Close sez me");
   if (timerMode) {
     Serial.println("Time to close door");
-    closeDoor();
+    startDoorMove(false);
   }
 }  
 
@@ -125,59 +137,55 @@ void setup()
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    
   }
   DateTime now = rtc.now();
   setTime(now.unixtime()); 
   motor.setSpeed(60);
   digitalClockDisplay();
   
-  Alarm.alarmRepeat(7, 0, 0, MorningOpen);  // 7am every day
-  Alarm.alarmRepeat(20, 0, 0, EveningClose );  // 8pm every night
-//  Alarm.alarmRepeat(0, 0, 20, MorningOpen);
-//  Alarm.alarmRepeat(0, 1, 0, EveningClose );
+//  Alarm.alarmRepeat(7, 0, 0, MorningOpen);  // 7am every day
+//  Alarm.alarmRepeat(20, 0, 0, EveningClose );  // 8pm every night
+ rtc.adjust(DateTime(2017, 1, 6, 0, 0, 0));
+  Alarm.alarmRepeat(0, 0, 20, MorningOpen);
+  Alarm.alarmRepeat(0, 1, 0, EveningClose );
+  digitalWrite(ledPin, LOW);
 }
 
 void loop()
 {
-  digitalWrite(ledPin, LOW);
-  if (Serial.available()) // for debugging only
-  {
-    int steps = Serial.parseInt();
-    Serial.println(steps);
-    motor.step(steps);
-    Alarm.delay(1000); // debounce
-    return;
-  }
-  
   // check if the pushbutton is pressed. If it is, the buttonState is LOW:
-  upButtonState = digitalRead(upButtonPin);
-  if (upButtonState == LOW) {
+  if (digitalRead(upButtonPin) == LOW) {
     digitalClockDisplay();
-    openDoor();
-    timerMode = false;
+    Serial.println("Opening door");
+    startDoorMove(true);
     Alarm.delay(500); // debounce
     return;
   }
   
-  downButtonState = digitalRead(downButtonPin);
-  if (downButtonState == LOW) {
+  if (digitalRead(downButtonPin) == LOW) {
     digitalClockDisplay();
-    closeDoor();
-    timerMode = false;
+    Serial.println("Closing door");
+    startDoorMove(false);
     Alarm.delay(500); // debounce
     return;
   }
 
-  timerButtonState = digitalRead(timerButtonPin);
-  if (timerButtonState == LOW) {
+  if (digitalRead(timerButtonPin) == LOW) {
     // switch to timer mode
     digitalClockDisplay();
     Serial.println("Timer mode enabled");
     timerMode = true;
+    doorInMotion = false;
+    digitalWrite(ledPin, LOW);
     Alarm.delay(500); // debounce
     return;
   }
-  Alarm.delay(100); // debounce
+
+  if (doorInMotion) {
+    Serial.println("Moving door a bit");
+    moveDoor();
+  }
+  Alarm.delay(1);
 }
 
